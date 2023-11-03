a399a7c0c94fefa36c8a13b29b4ba873c191dfec
An Byungwook <abw2619@icloud.com>
2023-11-03 15:14:58 +0900

    Initial Commit

---
+++ AppStatusTest/AppStatusTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStatusTestApp.swift
+//  AppStatusTest
+//
+//  Created by 안병욱의 mac on 2023/11/03.
+//
+
+import SwiftUI
+
+@main
+struct AppStatusTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
